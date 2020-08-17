cfc534835f0d6b0240d9a224de1031f5ec95def1
Anya Brun <tsbrun@icloud.com>
2020-08-17 17:20:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid? && sender.balance > amount
+    if sender.valid? && receiver.valid? && sender.balance > amount
       sender.balance -= amount
       receiver.balance += amount
 
