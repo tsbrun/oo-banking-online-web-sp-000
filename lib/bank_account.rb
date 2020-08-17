10d74ca6d61945f97aee9da48e23cf6c31422753
Anya Brun <tsbrun@icloud.com>
2020-08-17 17:02:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,15 @@
 class BankAccount
   attr_reader :name
-  attr_accessor :balance, :status 
+  attr_accessor :balance, :status
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = 'open'
+  end
+
+  def deposit(deposit)
+    @balance += deposit
   end
 
 end
